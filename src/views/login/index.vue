55ff3325b29498eb10ede1fcf95cd396160113db
laozhang <496301505@qq.com>
2019-10-25 11:27:15 +0800

    路由搭建

---
+++ src/views/login/index.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="container">
+        login
+    </div>
+</template>
+
+<script>
+export default {
+};
+</script>
+
+<style scoped lang="less">
+    .container {
+        position: absolute;
+        width: 100%;
+        height: 100%;
+        background: green;
+    }
+</style>
